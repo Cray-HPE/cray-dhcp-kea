fee09f30c599d1473ab89f4734f6183c7478a3f3
Dat Le <dle@cray.com>
2020-05-05 22:12:55 -0500

    cleaned up configmap files and removed extra files after consolidating to a single container

--- build_container_run_docker_compose.sh
+++ build_container_run_docker_compose.sh
@@ -1,6 +1,4 @@
 #/bin/bash
 
 docker build -t cray-dhcp-kea -f Dockerfile.dhcp-kea .
-docker build -t cray-dhcp-kea-ctrl-agent -f Dockerfile.dhcp-kea-ctrl-agent .
-docker build -t cray-dhcp-kea-init -f Dockerfile.dhcp-kea-init .
 docker-compose up -d
