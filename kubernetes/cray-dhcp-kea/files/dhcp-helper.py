39521cfefd671995b2a5b7527b436e6b9b656b19
Dat Le <dle@cray.com>
2020-06-10 08:58:57 -0500

    progress..

--- kubernetes/cray-dhcp-kea/files/dhcp-helper.py
+++ kubernetes/cray-dhcp-kea/files/dhcp-helper.py
@@ -33,6 +33,21 @@ kea_ipv4_leases = {}
 # }
 smd_ethernet_interfaces = {}
 
+# dict for sls hardware entry
+# 'x3000c0s19b1n0' : {
+#     "Parent":"x3000c0s19b1",
+#     "Xname":"x3000c0s19b1n0",
+#     "Type":"comptype_node",
+#     "Class":"River",
+#     "TypeString":"Node",
+#     "ExtraProperties":{
+#          "Aliases":["nid000001"],
+#          "NID":1,
+#           "Role":"Compute"
+#     }
+# }
+sls_hardware_entry = {}
+
 # 1) ##############################################################################
 
 #   a) Get network subnet and cabinet subnet info from SLS
@@ -74,14 +89,39 @@ for item in resp.json():
 
 for smd_mac_address in smd_ethernet_interfaces:
     # if SMD has MAC and IP and not in Kea DHCP reservation, add DHCP reservation in Kea
+    # if SMD has MAC and IP and not in Kea DHCP reservation, add DHCP reservation in Kea
     if smd_ethernet_interfaces[smd_mac_address]['IPAddress'] != '' and smd_mac_address not in kea_ipv4_leases:
-        print('Found MAC and IP address pair from SMD and updating Kea with the record: {} {}'.format(smd_mac_address, smd_ethernet_interfaces[smd_mac_address]['IPAddress']))
-        data = {'command': 'lease4-update', 'arguments': {'ip-address': smd_ethernet_interfaces[smd_mac_address]['IPAddress'], 'hw-address': smd_mac_address, 'force-create': 'true'}}
+        data = {'command': 'lease4-update','service': 'dhcp4', 'arguments': {'ip-address': smd_ethernet_interfaces[smd_mac_address]['IPAddress'], 'hw-address': smd_mac_address, 'hostname':smd_ethernet_interfaces[smd_mac_address]['ComponentID'],'force-create': 'true'}}
+        # check for alias
+        sls_hardware_url = 'http://cray-sls/v1/hardware/' + str(smd_ethernet_interfaces[smd_mac_address]['ComponentID']) + 'n0'
+        print(sls_hardware_url)
+        try:
+            resp = requests.get(url=sls_hardware_url)
+            resp.raise_for_status()
+        except Exception as err:
+            raise SystemExit(err)
+        print(resp.json()['ExtraProperties']['Aliases'])
+        if resp.json()['ExtraProperties']['Aliases'] != '':
+#            option_data = {}
+            data['arguments']['option-data'] = []
+            for alias in resp.json()['ExtraProperties']['Aliases']:
+                alias_hostname = {}
+                alias_hostname['space'] = "dhcp4"
+                alias_hostname['name'] = "hostname"
+                alias_hostname['code'] = "12"
+                alias_hostname['data'] = alias
+                data['arguments']['option-data'].append(alias_hostname)
+#            data.append(option_data)
+        # convert mac format
+        data['arguments']['hw-address'] = ':'.join(smd_mac_address[i:i+2] for i in range(0,12,2))
+        print(data)
+        print('Found MAC and IP address pair from SMD and updating Kea with the record: {} {} {}'.format(smd_mac_address, smd_ethernet_interfaces[smd_mac_address]['IPAddress'], smd_ethernet_interfaces[smd_mac_address]['ComponentID'],))
         try:
             resp = requests.post(url=kea_api_endpoint, json=data, headers=kea_headers)
             resp.raise_for_status()
         except Exception as err:
             raise SystemExit(err)
+        print(resp)
     # if IP Address is not present for a given mac address record in SMD, but Kea has a record with the MAC address and a non-empty IP, we can submit updates to SMD
     if smd_ethernet_interfaces[smd_mac_address]['IPAddress'] == '' and smd_mac_address in kea_ipv4_leases and kea_ipv4_leases[smd_mac_address]['ip-address'] != '':
         print("") # TODO: remove this print line, just here to ensure there's some code in the if block
@@ -91,3 +131,4 @@ for kea_mac_address in kea_ipv4_leases:
     # go through the Kea records now and make sure we submit any MACs/IPs that weren't in SMD at all back for discovery to SMD
     if kea_mac_address not in smd_ethernet_interfaces:
         # TODO: Submit IP for discovery in SMD
+        print("ready to submit to smd")
