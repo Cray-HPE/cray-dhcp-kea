1a7b210e3339905dc8ceefb532c50a4160fc44bd
Dat Le <dle@cray.com>
2020-06-12 09:36:11 -0500

    sending up whwere i am for gut-check

--- kubernetes/cray-dhcp-kea/files/dhcp-helper.py
+++ kubernetes/cray-dhcp-kea/files/dhcp-helper.py
@@ -59,6 +59,39 @@ sls_hardware_entry = {}
 # }
 lease_database_info = {}
 
+# array for subnet4
+#[
+#  {
+#    "pools": {
+#      "pool": "10.254.0.26-10.254.3.205"
+#    },
+#    "option-data": {
+#      "name": "router",
+#      "data": "10.254.0.1"
+#    },
+#    "subnet": "10.254.0.0/22"
+#  }
+#]
+resp = requests.get(url='http://cray-sls/v1/search/hardware?type=comptype_cabinet')
+subnet4 = []
+for item in resp.json()[0]['ExtraProperties']['Networks']['cn']:
+    subnet4_subnet = {}
+    subnet4_subnet['pools'] = {}
+    subnet4_subnet['pools']['pool'] = {}
+    subnet4_subnet['option-data'] = {}
+    cn_network_cidr = resp.json()[0]['ExtraProperties']['Networks']['cn'][item]['CIDR']
+    cn_gateway = resp.json()[0]['ExtraProperties']['Networks']['cn'][item]['Gateway']
+    cn_network = ipaddress.ip_network(cn_network_cidr)
+    cn_network_total_hosts = cn_network.num_addresses
+    cn_network_pool_start = cn_network[26]
+    cn_network_pool_end = cn_network[cn_network_total_hosts -51]
+    #create dictionary json for subnet
+    subnet4_subnet['subnet'] = cn_network_cidr
+    subnet4_subnet['pools']['pool'] = str(cn_network_pool_start) + '-' + str(cn_network_pool_end)
+    subnet4_subnet['option-data']['name'] = 'router'
+    subnet4_subnet['option-data']['data'] = cn_gateway
+    subnet4.append(subnet4_subnet)
+
 data = {'command': 'config-get', 'service': ['dhcp4']}
 kea_headers = {'Content-Type': 'application/json'}
 kea_api_endpoint = 'http://cray-dhcp-kea-api:8000'
