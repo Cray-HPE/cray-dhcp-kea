542ace8f8f32e00e7caa6870155087ef96a18c5f
Dat Le <dle@cray.com>
2020-06-16 18:09:37 -0500

    more fixes to subnet object

--- kubernetes/cray-dhcp-kea/files/dhcp-helper.py
+++ kubernetes/cray-dhcp-kea/files/dhcp-helper.py
@@ -105,19 +105,18 @@ for item in resp.json()[0]['ExtraProperties']['Networks']['cn']:
     subnet4_subnet = {}
     subnet4_subnet['pools'] = []
     subnet4_subnet['pools'].append({'pool': {}})
-    subnet4_subnet['option-data'] = {}
+    subnet4_subnet['option-data'] = []
     cn_network_cidr = resp.json()[0]['ExtraProperties']['Networks']['cn'][item]['CIDR']
     cn_gateway = resp.json()[0]['ExtraProperties']['Networks']['cn'][item]['Gateway']
     cn_network = ipaddress.ip_network(cn_network_cidr)
     cn_network_total_hosts = cn_network.num_addresses
-    cn_network_pool_start = cn_network[26]                                             
+    cn_network_pool_start = cn_network[26]
     cn_network_pool_end = cn_network[cn_network_total_hosts -51]
     #create dictionary json for subnet
     subnet4_subnet['subnet'] = cn_network_cidr
     subnet4_subnet['pools'][0]['pool'] = str(cn_network_pool_start) + '-' + str(cn_network_pool_end)
-    subnet4_subnet['option-data']['name'] = 'router'
-    subnet4_subnet['option-data']['data'] = cn_gateway
-    subnet4_subnet['pools'].append(subnet4_subnet['pools'][0]['pool'])
+    subnet4_subnet['option-data'].append({'name': 'routers', 'data': cn_gateway})
+#    subnet4_subnet['pools'].append(subnet4_subnet['pools'][0]['pool'])
     subnet4.append(subnet4_subnet)
 cray_dhcp_kea_dhcp4['Dhcp4']['subnet4'].extend(subnet4)
 
