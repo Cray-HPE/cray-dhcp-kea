0d258c41699e059d9b3a006840aab7bc3c836f00
Dat Le <dle@cray.com>
2020-06-16 13:31:28 -0500

    fixing startup and removing jobs.yaml

--- startup-dhcp.sh
+++ startup-dhcp.sh
@@ -49,4 +49,4 @@ sed -i 's/EOF//g' /usr/local/kea/cray-dhcp-kea-dhcp4.conf
 #  helpful for debugging
 cat /usr/local/kea/cray-dhcp-kea-dhcp4.conf
 # what we use to run Cray DHCP Kea server
-/usr/local/sbin/kea-dhcp4 -c /usr/local/kea/cray-dhcp-kea-dhcp4.conf && while true; do "python3 dhcp-helper.py"; sleep 900; done
+/usr/local/sbin/kea-dhcp4 -c /usr/local/kea/cray-dhcp-kea-dhcp4.conf && 'while true; do "python3 dhcp-helper.py"; sleep 900; done'
