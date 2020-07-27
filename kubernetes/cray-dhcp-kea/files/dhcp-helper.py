e958110bf9fa20ed4460c8ec14b499377090929e
Dat Le <dle@cray.com>
2020-07-27 09:48:21 -0500

    updated logic for entering data in SMD table and fixed race condition on health check(CASMNET-217

--- kubernetes/cray-dhcp-kea/files/dhcp-helper.py
+++ kubernetes/cray-dhcp-kea/files/dhcp-helper.py
@@ -250,7 +250,7 @@ for mac_address, mac_details in kea_ipv4_leases.items():
         except Exception as err:
             on_error(err)
         # we update SMD only if ip doesn't exec or special case where HSM discovery does not discover the hostname
-        if search_smd_ip_resp.json() == [] or (len(search_smd_ip_resp.json()) == 1 and kea_hostname != '' and kea_ip == search_smd_ip_resp.json()[0]['IPAddress'] and smd_mac_format == search_smd_ip_resp.json()[0]['MACAddress']):
+        if search_smd_ip_resp.json() == [] or (len(search_smd_ip_resp.json()) == 1 and kea_ip == search_smd_ip_resp.json()[0]['IPAddress'] and smd_mac_format == search_smd_ip_resp.json()[0]['MACAddress']):
             update_smd_url = 'http://cray-smd/hsm/v1/Inventory/EthernetInterfaces'
             post_data = {'MACAddress': smd_mac_format, 'IPAddress': kea_ip, 'ComponentID': kea_hostname}
             try:
--- kubernetes/cray-dhcp-kea/files/health-check.sh
+++ kubernetes/cray-dhcp-kea/files/health-check.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # check number of pids for kea
-health=$(ps -ef |grep kea|wc -l)
+health=$(ps -ef |grep kea|grep-v grep|wc -l)
 
 # need greater than
-if [[ $health -gt 1 ]]
+if [[ $health -gt 0 ]]
 then
   exit 0
 else
