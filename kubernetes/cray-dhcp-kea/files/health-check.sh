1cc7d9277b42eb536b652d9bf1641f2e26bea736
Dat Le <dle@cray.com>
2020-07-27 20:54:51 -0500

    fixed syntax on health check

--- kubernetes/cray-dhcp-kea/files/health-check.sh
+++ kubernetes/cray-dhcp-kea/files/health-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # check number of pids for kea
-health=$(ps -ef |grep kea|grep-v grep|wc -l)
+health=$(ps -ef |grep kea|grep -v grep|wc -l)
 
 # need greater than
 if [[ $health -gt 0 ]]
