a2821fdaa1312e6574d9eaf28ab063ad1179259f
Dat Le <dle@cray.com>
2020-08-05 13:59:37 -0500

    edge case in lease4-get-all where if no leases are active, false positive test.  changed to config-get

--- kubernetes/cray-dhcp-kea/files/health-check.sh
+++ kubernetes/cray-dhcp-kea/files/health-check.sh
@@ -5,7 +5,7 @@ pid_health=$(ps -ef |grep kea|grep -v grep|wc -l)
 # check status via api
 # result = 0 means kea server and api is working
 # result != 0 means something is wrong
-kea_health=$(curl -X POST -H "Content-Type: application/json" -d '{ "command": "lease4-get-all",  "service": [ "dhcp4" ] }' cray-dhcp-kea-api:8000|jq '.[].result')
+kea_health=$(curl -X POST -H "Content-Type: application/json" -d '{ "command": "config-get",  "service": [ "dhcp4" ] }' cray-dhcp-kea-api:8000|jq '.[].result')
 
 if [[ $pid_health -gt 0 ]] && [[ $kea_health -eq 0 ]]
 then
