30263a1f45398bd3d9a65de2ca4cb9c2b301d56b
Dat Le <dle@cray.com>
2021-01-29 13:16:44 -0600

    switch from array to tuple

--- kubernetes/cray-dhcp-kea/files/dhcp-helper.py
+++ kubernetes/cray-dhcp-kea/files/dhcp-helper.py
@@ -268,7 +268,7 @@ for name in system_name:
 
 if not dnsmasq_running:
     for i in range(len(sls_networks)):
-        if any(n in sls_networks[i]['Name'] for n in ['NMN','HMN','MTL','CAN']):
+        if any(n in sls_networks[i]['Name'] for n in ('NMN','HMN','MTL','CAN')):
             if 'Subnets' in sls_networks[i]['ExtraProperties'] and sls_networks[i]['ExtraProperties']['Subnets']:
                 for system in sls_networks[i]['ExtraProperties']['Subnets']:
                     if sls_networks[i]['Name'] == 'NMN':
@@ -292,7 +292,7 @@ if not dnsmasq_running:
                         subnet4_subnet['id'] = system['VlanID']
                         subnet4_subnet['reservation-mode'] = 'all'
                         subnet4_subnet['reservations'] = []
-                        if any(n in sls_networks[i]['Name'] for n in ['NMN','MTL','CAN']):
+                        if any(n in sls_networks[i]['Name'] for n in ('NMN','MTL','CAN')):
                             subnet4_subnet['option-data'].append({'name': 'domain-name-servers','data': unbound_servers['NMN']})
                             subnet4_subnet['next-server'] = tftp_server_nmn
                             subnet4_subnet['option-data'].append({'name': 'time-servers', 'data': str(time_servers_nmn).strip('[]') })
