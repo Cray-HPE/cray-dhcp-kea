19fcd548265b68b59d49d981a4db2d204d067669
Dat Le <dle@cray.com>
2021-10-19 20:42:53 -0500

    cleanup of generated configs

--- kubernetes/cray-dhcp-kea/files/startup-dhcp-ctrl-agent.sh
+++ kubernetes/cray-dhcp-kea/files/startup-dhcp-ctrl-agent.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-( echo "cat <<EOF" ; cat /srv/kea/cray-dhcp-kea-ctrl-agent.conf  ; echo EOF ) | sh > /usr/local/etc/kea/cray-dhcp-kea-ctrl-agent.conf
+( echo "cat <<EOF" ; cat /srv/kea/cray-dhcp-kea-ctrl-agent.conf  ; echo EOF ) | sh > /usr/local/kea/cray-dhcp-kea-ctrl-agent.conf
+sed -i 's/EOF//g' /usr/local/kea/cray-dhcp-kea-ctrl-agent.conf
 #  helpful for debugging
 cat /usr/local/kea/cray-dhcp-kea-ctrl-agent.conf
 # what we use to run Cray Dhcp Kea control agent(api)
