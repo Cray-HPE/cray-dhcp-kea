4dce04e506a0b8502f788cb403bb38133837e074
Dat Le <dle@cray.com>
2021-10-21 15:33:38 -0500

    move to none protected port for none root user

--- kubernetes/cray-dhcp-kea/files/startup-dhcp.sh
+++ kubernetes/cray-dhcp-kea/files/startup-dhcp.sh
@@ -11,6 +11,6 @@ cp /srv/kea/startup-config-dhcp4.conf /usr/local/kea/cray-dhcp-kea-dhcp4.conf.ba
 #  helpful for debugging
 cat /usr/local/kea/cray-dhcp-kea-dhcp4.conf
 # what we use to run Cray DHCP Kea server
-nohup /usr/local/sbin/kea-dhcp4 -c /usr/local/kea/cray-dhcp-kea-dhcp4.conf &
+nohup /usr/local/sbin/kea-dhcp4 -p 6067 -c /usr/local/kea/cray-dhcp-kea-dhcp4.conf &
 # we will need to tune this
 while true; do /srv/kea/dhcp-helper.py; sleep ${DHCP_HELPER_INTERVAL_SECONDS}; done
