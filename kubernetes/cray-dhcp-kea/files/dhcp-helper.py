1c3a0d693789b096f12385ef68fd727e6b34f5d5
Dat Le <dle@cray.com>
2022-03-15 09:31:18 -0500

    fix syntax format for deprication message in python

--- kubernetes/cray-dhcp-kea/files/dhcp-helper.py
+++ kubernetes/cray-dhcp-kea/files/dhcp-helper.py
@@ -62,7 +62,7 @@ class APIRequest(object):
             total=10,
             backoff_factor=0.1,
             status_forcelist=[429, 500, 502, 503, 504],
-            method_whitelist=["PATCH", "DELETE", "POST","HEAD", "GET", "OPTIONS"]
+            allowed_methods=["PATCH", "DELETE", "POST","HEAD", "GET", "OPTIONS"]
         )
 
         adapter = HTTPAdapter(max_retries=retry_strategy)
