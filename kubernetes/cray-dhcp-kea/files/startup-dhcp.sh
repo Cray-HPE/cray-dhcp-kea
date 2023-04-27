4a6224be5eff794a20429046d005a9092a6a6f61
dle-hpe <dat.le@hpe.com>
2023-04-27 13:26:08 -0500

    - CASMNET-2107 - addional comments - CASMTRIAGE-5222 - do not start kea until config has been generated - CASMNET-1752 - validate generated kea config abort if invalid config - CASMNET-2108 - add kea config backup and load from backup functionality to cray-dhcp-kea - CASMNENT-2109 - add per node pxe options - CASMNET-1525 - Control dhcp-helper.py log levels via an environment variable

--- kubernetes/cray-dhcp-kea/files/startup-dhcp.sh
+++ kubernetes/cray-dhcp-kea/files/startup-dhcp.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
-# wait for istio
 
+# wait for istio
 until curl --head localhost:15000  ; do echo Waiting for Sidecar; sleep 3 ; counter++ ; done ; echo Sidecar available;
 
-#mkdir -p /usr/local/kea
-#  since the username and password is dynamic with the k8s postgres operator
-#  we output the config file and substitute the environment variables
-cp /srv/kea/startup-config-dhcp4.conf /usr/local/kea/cray-dhcp-kea-dhcp4.conf
-cp /srv/kea/startup-config-dhcp4.conf /usr/local/kea/cray-dhcp-kea-dhcp4.conf.bak
-#  helpful for debugging
-cat /usr/local/kea/cray-dhcp-kea-dhcp4.conf
+BACKUP_CONFIG_PATH=/srv/kea/backup/
+BACKUP_CONFIG_FILE=keaBackup.conf.gz
+
+if [ -f "$BACKUP_CONFIG_PATH$BACKUP_CONNFIG" ]; then
+    echo "Backup exists."
+    echo "Validating"
+    cd /tmp
+    cp $BACKUP_CONFIG_PATH$BACKUP_CONNFIG .
+    gunzip "${BACKUP_CONFIG_FILE%.*}"
+    kea_dhcp4 -t "${BACKUP_CONFIG_FILE%.*}"
+    if
+fi
+
+# If no backup was loaded.  Run initialization of configs via dhcp-helper.py
+if [ ! -f "/usr/local/kea/cray-dhcp-kea-dhcp4.conf" ]; then
+    # init run of dhcp-helper
+    /srv/kea/dhcp-helper.py --init
+    #  we output the config file and substitute the environment variables
+    cp /usr/local/kea/cray-dhcp-kea-dhcp4.conf /usr/local/kea/cray-dhcp-kea-dhcp4.conf.bak
+else
+    echo "ERROR: no config file loaded or initialized."
+fi
+
 # what we use to run Cray DHCP Kea server
 nohup /usr/local/sbin/kea-dhcp4 -p 6067 -P 67 -c /usr/local/kea/cray-dhcp-kea-dhcp4.conf &
+
 # kea exporter for prometheus
 kea-exporter --address ${KEA_EXPORTER_ADDRESS} --port ${KEA_EXPORTER_PORT} ${KEA_SOCKET} &
+
 # we will need to tune this
 while true; do /srv/kea/dhcp-helper.py; sleep ${DHCP_HELPER_INTERVAL_SECONDS}; done
--- kubernetes/cray-dhcp-kea/templates/_helpers.tpl
+++ kubernetes/cray-dhcp-kea/templates/_helpers.tpl
@@ -1,3 +1,33 @@
+{{/* vim: set filetype=mustache: */}}
 {{/*
-Add helper methods here for your chart
+
+{{/*
+Expand the name of the chart.
 */}}
+{{- define "cray-dhcp-kea.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Create a fully qualified app name
+*/}}
+{{- define "cray-dhcp-kea.fullname" -}}
+{{- if .Values.fullnameOverride -}}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+
+
+{{- define "cray-dhcp-kea.labels" }}
+helm.sh/chart: {{ include "cray-dhcp-kea.chart" . }}
+app.kubernetes.io/name: {{ include "cray-dhcp-kea.name" . }}
+app.kubernetes.io/instance: {{ .Release.Name }}
+app.kubernetes.io/managed-by: {{ .Release.Service }}
+{{- end -}}
